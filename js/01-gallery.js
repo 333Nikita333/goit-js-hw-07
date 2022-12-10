845d247d6e29c14bc71a95af3b80f5b0d01814fe
333Nikita333 <nikitosfox333@gmail.com>
2022-12-10 16:03:48 +0200

    created image gallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryImagesEl = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryMarkup(galleryItems);
+addGallery(galleryItemsMarkup);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      item => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+  </div>`,
+    )
+    .join('');
+}
+function addGallery(markup) {
+  galleryImagesEl.innerHTML = markup;
+}
